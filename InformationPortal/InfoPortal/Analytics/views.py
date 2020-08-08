632616bf00b4c9d935976af4265ded13a953c1bc
ravitejajagadish <ja.raviteja.11@gmail.com>
2020-08-08 23:07:30 +0530

    added restApi processing for analytics

--- InformationPortal/InfoPortal/Analytics/views.py
+++ InformationPortal/InfoPortal/Analytics/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 import requests
+import json
 
 def my_django_view(request,*args,**kwargs):
+    response = None
     try :
-        response = requests.post('https://www.somedomain.com/some/url/save', params=request.POST)
-        print(response.status_code)
-        print(response)
-    except:
-        pass
-    finally: 
+        response = requests.post('https://www.somedomain.com/some/url/save', params=request.POST) 
+    except: 
         return render(request,"analytics.html",{})
-# Create your views here.
+    else:
+        json = json.load(response)
+        data = json.data
+        return render(request,"analytics.html",{"onion" : data[0], "potato" : data[1], "Carrot" : data[2]})
 
