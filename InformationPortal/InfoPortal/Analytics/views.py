2e2c55856ee6d2aeccd0c8e64f7f121173ff7d88
ravitejajagadish <ja.raviteja.11@gmail.com>
2020-08-08 20:18:34 +0530

    3 axis chart

--- InformationPortal/InfoPortal/Analytics/views.py
+++ InformationPortal/InfoPortal/Analytics/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-#import requests
+import requests
 
 def my_django_view(request,*args,**kwargs):
-    # if request.method == 'POST':
-    #     r = requests.post('https://www.somedomain.com/some/url/save', params=request.POST)
-    # else:
-    #     r = requests.get('https://www.somedomain.com/some/url/save', params=request.GET)
-    # if r.status_code == 200:
-    #     return render(request,'Yay, it worked')
-    return render(request,"template.html",{})
+    try :
+        response = requests.post('https://www.somedomain.com/some/url/save', params=request.POST)
+        print(response.status_code)
+        print(response)
+    except:
+        pass
+    finally: 
+        return render(request,"template.html",{})
 # Create your views here.
+
