ba7789b1757ef31464ed311bc0ca45e2f7e09f83
harshakunchala <harshavvardhan602@gmail.com>
2020-08-09 08:26:07 +0530

    added bidding part

--- Frontend/dealer/dealers.php
+++ Frontend/dealer/dealers.php
@@ -91,20 +91,23 @@
               
                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
                <a class="block relative h-48 rounded overflow-hidden">
-                 <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="$image">
-               </a>
+                 <img class="object-cover object-center w-full h-full block" src="../$image">
+                //  echo '<img class="object-cover object-center w-full h-full block" src="C:/xampp1/htdocs/codeforcure/uploads/bg.jpg">';
+                 </a>
                <div class="mt-4">
                  <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">$category</h3>
                  <h2 class="text-gray-900 title-font text-lg font-medium">IRITY-KERALA</h2>
                  <div class="flex flex-col justify-center">
-                   <p class="mt-1">Base :- Rs $price</p>
-                   <p class="mt-1">Current :- Rs 50.00</p>
+                   <p class="mt-1">Base :- Rs 100</p>
+                   <p class="mt-1">Current :- $price</p>
                    </div>
                    <br>
+                   <form action="/codeforcure/bid.php/" method="post" >
                    <div class="flex flex-row">
-                     <input class="justify-center bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" placeholder="Enter the bid amount" type="number">
+                     <input  name="name" class="justify-center bg-white rounded border border-gray-400 focus:outline-none focus:border-indigo-500 text-base px-4 py-2 mb-4" placeholder="Enter the bid amount" type="number">
                      <button class="flex ml-auto text-white bg-indigo-500 border-0 py-1 px-4 focus:outline-none hover:bg-indigo-600 rounded">Bid</button>        
                    </div>
+                   </form>
                </div>
              </div>
        
